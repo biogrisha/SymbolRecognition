5ba03afaa51e12f7e4d4e3848c39a3b865c8b83a
Grigorii Aspiotis <grigorii.aspiotis@accenture.com>
2022-04-02 03:44:59 +0300

    Added a function to calculate nested sums and get the number denoted by the image

--- SymbolRecognition/SymbolRecognition.cpp
+++ SymbolRecognition/SymbolRecognition.cpp
@@ -11,10 +11,89 @@
 using namespace std;
 vector<float> coefs;
 
-//vector<bool> img = { 11110000 };
 
 
+SuperInt EvalNestedSeriesSumm(int value, int nestLevel)
+{
+    SuperInt unit(2, 1);
+    SuperInt factor(2, 1);
 
+    SuperInt dividend(20, value);
+    SuperInt dividendRes(20, value);
+    SuperInt divisor(2, 1);
+
+    if (nestLevel <1)
+    {
+        dividendRes = unit;
+    }
+
+    for (int i = 1; i < nestLevel; i++)
+    {
+        dividend = dividend.Sum(unit);
+        dividendRes = dividendRes.Mult(dividend);
+
+        factor = factor.Sum(unit);
+        divisor = divisor.Mult(factor);
+
+    }
+
+    dividendRes = dividendRes.Div(divisor);
+
+    return dividendRes;
+}
+
+
+vector<bool> img = { 1,1,0,0,0,0,0,0};
+
+SuperInt GetImageNum(const vector<bool>& img)
+{
+    SuperInt res(2, 1);
+    int unitsNum = 0;
+    int imgSize = img.size();
+    for (bool unit : img)
+    {
+        unitsNum += unit;
+    }
+
+    if (unitsNum < img.size() / 2)
+    {
+        int nestLev = 0;
+
+        for (int i = 1; i < unitsNum; i++)
+        {
+            res = res.Sum(EvalNestedSeriesSumm((imgSize + 1) - i, i));
+            nestLev = i;
+        }
+
+
+        int PrevElPos = -1;
+        int distFromPrev = 0;
+        int maxNum = imgSize - (unitsNum - 1);
+        for (int i = 0; i < imgSize; i++)
+        {
+            if (img[i])
+            {
+
+                if ((i - PrevElPos) > 1)
+                {
+                    for (int j = PrevElPos + 1; j < i; j++)
+                    {
+                        res = res.Sum(EvalNestedSeriesSumm(maxNum, nestLev));
+
+                        maxNum--;
+                    }
+                }
+
+                nestLev--;
+                PrevElPos = i;
+            }
+
+        }
+        res.PrintDecimal();
+        cout << endl;
+    }
+    return res;
+}
 
 float getSum()
 {
@@ -77,5 +156,5 @@ float GetFuncRes(float x, float tolerance, int polDeg)
 
 int main(int argc, char** argv)
 {
-
+    GetImageNum(img);
 }
